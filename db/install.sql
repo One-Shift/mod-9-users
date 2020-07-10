3321aa18d310302a3ee4a4b89abc958acb90cd89
Carlos <Santos>
2020-07-10 12:23:51 +0100

    update c2r for install

--- db/install.sql
+++ db/install.sql
@@ -1,6 +1,6 @@
-INSERT INTO `{c2r-prefix}_modules` (`name`, `folder`, `code`) VALUES ("{c2r-mod-name}", "{c2r-mod-folder}", '{\"fa-icon\":\"fa-users\",\"img\":\"\",\"sub-items\":{\"List\":{\"url\":\"\"},\"Add user\":{\"url\":\"add\"},\"Logs\":{\"url\":\"logs\"}},\"sidebar\":true,\"dropdown\":false}');
+INSERT INTO `{{ prefix }}_modules` (`name`, `folder`, `code`) VALUES ("{{ mod-name }}", "{c2r-mod-folder}", '{\"fa-icon\":\"fa-users\",\"img\":\"\",\"sub-items\":{\"List\":{\"url\":\"\"},\"Add user\":{\"url\":\"add\"},\"Logs\":{\"url\":\"logs\"}},\"sidebar\":true,\"dropdown\":false}');
 
-CREATE TABLE `{c2r-prefix}_9_users` (
+CREATE TABLE `{{ prefix }}_9_users` (
 	`id` int(11) NOT NULL,
 	`username` varchar(255) DEFAULT NULL,
 	`password` varchar(255) DEFAULT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE `{c2r-prefix}_9_users` (
 	`date_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE `{c2r-prefix}_9_users_fields` (
+CREATE TABLE `{{ prefix }}_9_users_fields` (
 	`id` int(11) NOT NULL,
 	`name` text NOT NULL,
 	`value` text NOT NULL,
@@ -27,14 +27,14 @@ CREATE TABLE `{c2r-prefix}_9_users_fields` (
 	`date_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-ALTER TABLE `{c2r-prefix}_9_users`
+ALTER TABLE `{{ prefix }}_9_users`
 	ADD PRIMARY KEY (`id`),
 	ADD UNIQUE KEY `name` (`username`),
 	ADD UNIQUE KEY `email` (`email`);
 
-ALTER TABLE `{c2r-prefix}_9_users_fields`
+ALTER TABLE `{{ prefix }}_9_users_fields`
 	ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `{c2r-prefix}_9_users`
+ALTER TABLE `{{ prefix }}_9_users`
 	MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
